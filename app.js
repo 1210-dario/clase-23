3c27bb424c2de1c5b6e40ad60a90d260313b6b1f
Dario Castromonte <dario.castromonte@gmail.com>
2021-05-28 15:21:55 -0300

    agregue iniciador de express

--- app.js
+++ app.js
@@ -7,3 +7,13 @@
 //crear archivo .gitignore 
 
 //En el archivo .gitignore ponemos los archivos/carpetas que queremos que git no siga, es decir ignora esos recursos 
+
+//Requiero Express
+const express = require('express');
+//Inicio express, es decir su ejecucion
+const app = express();
+//Declaro el puerto
+const port = 3000;
+//Inicio el servidor en el puerto indicado con un callback
+app.listen(port, ()=>console.log(`Servidor Iniciado en el puerto: ${port}`)
+);
