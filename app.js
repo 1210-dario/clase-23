567abeaccb97a45fdfdd0e2353fb7721738e3e28
Dario Castromonte <dario.castromonte@gmail.com>
2021-05-28 14:52:33 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Inicio Proyecto con npm init
+
+//Luego hago npm i express para instalar express
+
+//git init para crear el repo local
+
+//crear archivo .gitignore 
+
+//En el archivo .gitignore ponemos los archivos/carpetas que queremos que git no siga, es decir ignora esos recursos 
